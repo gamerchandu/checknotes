904c8cbe1f0b0fce6c8a047a8b5cf296929cbbc1
chandra ch <chchandu123@gmail.com>
2024-03-26 23:54:03 +0530

    update

--- tests/orangeLogin.spec.ts
+++ tests/orangeLogin.spec.ts
@@ -3,5 +3,4 @@ import {  test, expect } from '@playwright/test';
     test('Login into OrangeHRM site', async ({page}) => {
         await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
         await expect(page.url()).toEqual('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-        await expect(page.getByLabel('Login')).toHaveText('Login');
     });
